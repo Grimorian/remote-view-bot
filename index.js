031f2913c68b00a3af8450b5a06fb96d11983102
grimorian <grimorian@grimorian.com>
2019-12-30 06:08:25 -0600

    welcome message and other updates

--- index.js
+++ index.js
@@ -7,27 +7,42 @@ const express = require('express'),
 app = express()
 port = process.env.PORT || 8080,
 botkey = process.env.BOTKEY || ':',
-url = `https://api.telegram.org/bot${ botkey }/sendMessage`
+url = `https://api.telegram.org/bot${ botkey }`
 
+let chats = {}
 
 app.get('/', (req, res) => {
     res.send('Remote Viewer Bot API')
 })
 
 app.post('/', jsonParser, (req, res) => {
+    console.log('Update received')
     console.log(req.body)
+    
+    const chatId = req.body.message.chat.id,
+    name = req.body.message.from.first_name
+
+    if(chats[chatId]) {
+      sendMessage(chatId, `Welcome back, ${ name }!`)
+    } else {
+      sendMessage(chatId, 
+      `Hello ${ name }, I am the Remote View bot. 
+      You can use the following commands:
+      /target     To specify a new target (random picture)
+      /reveal     To reveal the target`)
+      chats[chatId] = { target : undefined }
+    }
+    
     res.send(req.body)
 })
 
-app.post('/target', jsonParser, target)
-
 app.listen(port, () => console.log(`Listening on port ${port}`))
 
-function target(req, res) {
+function sendMessage(chatId, text, callback) {
   request
-  .post(url)
-  .form({ chat_id: req.body.message.chat.id, text: 'Greetings from the server ' + JSON.stringify(req.body.message) })
-  .on('response', response => res.send('Test'))
+  .post(`${ url }/sendMessage`)
+  .form({ chat_id: chatId, text })
+  .on('response', response => callback && callback(response))
 }
 
 
